6c6bfdfebf97b1ec01dddac3e6297c7540cfdb2f
Kaitlin Ramirez <kramirez@avvo.com>
2018-11-12 13:24:50 -0800

    working so far

---
+++ src/index.js
@@ -0,0 +1,51 @@
+const { ApolloServer } = require('apollo-server')
+const { MongoClient } = require('mongodb')
+require('dotenv').config()
+
+const typeDefs = `
+  type Photo {
+    id: ID!
+    name: String!
+    description: String
+  }
+
+  type Mutation {
+    postPhoto(name: String! description: String): Photo!
+  }
+  type Query {
+    totalPhotos: Int!
+  }
+`
+
+const resolvers = {
+  Query: {
+    totalPhotos: (parent, args, { photos }) => photos.countDocuments()
+    // countDocuments is a mongodb function for length
+  },
+  Mutation: {
+    postPhoto: async (parent, args, { photos }) => {
+      const newPhoto = {...args}
+      const {insertedId} = await photos.insertOne(newPhoto)
+      newPhoto.id = insertedId.toString()
+      return newPhoto
+    }
+  }
+}
+
+const start = async () => {
+  const client = await MongoClient.connect(process.env.POTATO, { useNewUrlParser: true })
+
+  const db = client.db()
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: {
+      photos: db.collection('photos')
+    }
+  })
+
+  server.listen().then(console.log)
+}
+
+start()
